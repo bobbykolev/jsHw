1fa57775453c155c3c5f3bd9b5b62575d7a2812a
Bobby Kolev <bobbykolev@abv.bg>
2013-04-26 01:37:07 +0300

    correction

--- 09.DOM/03.querySelector/script.js
+++ 09.DOM/03.querySelector/script.js
@@ -3,7 +3,7 @@ window.onload = function() {
     	
     	document.getElementById('wrapper').innerHTML = "";//clear divs on execute
         var wrapper = document.querySelectorAll('body>#wrapper.one')[0];
-        var text = document.createTextNode(" Lorem ipsum dolar... ");
+        var text = document.createTextNode(" It works... ");
         wrapper.appendChild(text);
         wrapper.style.color = "#FFFFFF";
         document.body.style.backgroundColor = "#000000";
--- 09.DOM/05.tagCloud/script.js
+++ 09.DOM/05.tagCloud/script.js
@@ -1,31 +1,35 @@
 window.onload = function() {
 
 	document.getElementById("enter").onclick = function() {
-		document.getElementById('wrapper').innerHTML = "";
+		document.getElementById('theUl').innerHTML = "";
+		document.getElementsByTagName('section')[0].innerHTML = "";
 		var arr = [];
 		var counter = 0;
 		var input = document.getElementById("area").value;
 		if (input) {
 			 arr = input.split(",");
 		}else{
-			 arr = ["wordpress", "joomla", "sitefinity", "drupal", "wordpress", "wordpress", "joomla", "sitefinity", "wordpress"];
+			 //arr = ["wordpress", "joomla", "sitefinity", "drupal", "wordpress", "wordpress", "joomla", "sitefinity", "wordpress"];
+			 arr = ["cms", "javascript", "js", "ASP.NET MVC", ".net", ".net", "css", "wordpress", "xaml", "js", "http", "web", "asp.net", "asp.net MVC", "ASP.NET MVC", "wp", "javascript", "js", "cms", "html", "javascript", "http", "http", "CMS"];
 		}
 	
 		arr = arr.sort();
 		for (var i = 0, l = arr.length; i < l; i++) {
-			if (arr[i] === arr[i + 1]) {
-				counter += 2;
+			if (arr[i].toLowerCase() === arr[i + 1].toLowerCase()) {
+				counter += 3;
 			} else {
 				var listItem = document.createElement("li"),
 					anchor = document.createElement("a"),
-					wrapper = document.getElementById("wrapper"),
-					li = wrapper.appendChild(listItem),
-					a = li.appendChild(anchor)
+					theUl = document.getElementById("theUl"),
+					li = theUl.appendChild(listItem),
+					a = li.appendChild(anchor),
 					txt = document.createTextNode(arr[i]);
 				a.appendChild(txt);
 				a.style.fontSize = (14 + counter) + "px";
 				a.style.color = getRandomColor();
 				a.setAttribute('href','#');
+				a.style.marginTop = getRandomNumber(5, 20) + "px";
+				a.style.marginLeft = getRandomNumber(5, 20) + "px";
 				counter = 0;
 			}
 		}
